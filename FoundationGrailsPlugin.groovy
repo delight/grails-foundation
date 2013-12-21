e6a7751b7efc79850179f55fc0b74413448b2ea6
Burt Beckwith <burt@burtbeckwith.com>
2013-12-21 12:56:21 -0500

    cleanup

--- FoundationGrailsPlugin.groovy
+++ FoundationGrailsPlugin.groovy
@@ -14,71 +14,20 @@
  * limitations under the License.
  */
 class FoundationGrailsPlugin {
-    // the plugin version
     def version = "5.0.2"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
-
     def title = "Foundation CSS Framework for Grails"
-    def author = "Konstantinos Kostarellis"
-    def authorEmail = "kosta.grails@gmail.com"
     def description = '''\
-Plugin that provides the Foundation CSS framework resources. Foundation is an easy to use, powerful, and flexible framework for building prototypes and production code on any kind of device.
+Provides the Foundation CSS framework resources. Foundation is an easy to use, powerful, and flexible framework for building prototypes and production code on any kind of device.
 It integrates nicely with the resources plugin.
 '''
 
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/foundation"
 
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-    def issueManagement = [ system: "GITHUB", url: "https://github.com/delight/grails-foundation/issues" ]
-
-    // Online location of the plugin's browseable source code.
-    def scm = [ url: "https://github.com/delight/grails-foundation" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+    def developers = [
+        [name: "Konstantinos Kostarellis", email: "kosta.grails@gmail.com"]
+    ]
+    def issueManagement = [system: "GITHUB", url: "https://github.com/delight/grails-foundation/issues"]
+    def scm = [url: "https://github.com/delight/grails-foundation"]
 }
